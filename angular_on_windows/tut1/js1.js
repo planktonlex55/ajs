05cc3a2c46df1abf8969685b176efab63876d193
planktonlex55 <planktonlex55@gmail.com>
2016-10-14 12:04:31 +0530

    Add files via upload

---
+++ angular_on_windows/tut1/js1.js
@@ -0,0 +1,13 @@
+
+var myApp = angular.module("myModule", [])
+					.controller("myController", function($scope){
+						$scope.message = "hello crappy happy world";
+					});
+/*
+var myController = myApp.controller("myController", function($scope) {
+	$scope.message = "Hello!";
+});
+
+/// <reference path="angular.js" />
+
+*/
---
+++ angular_on_windows/tut1/readme1.txt
@@ -0,0 +1,7 @@
+created C:\xampp\htdocs\z_radical in C:\xampp\htdocs\
+Add 1.html to z_radical/
+Add js1.js to z_radical/
+restart apache on xampp
+
+goto http://127.0.0.1/z_radical/1.html on chrome.
+o/p : some crap aint it xxx ? hello crappy happy world
