7dcfa105c7f58a4873193674c619ec95a10a905c
planktonlex55 <planktonlex55@gmail.com>
2016-12-15 11:03:25 +0530

    Create obj_manipulation.js

---
+++ javascript/advanced_js/obj_manipulation.js
@@ -0,0 +1,40 @@
+//object manipulation.
+//everything in JS is a fckung obj. like python.
+
+var Pizza = {
+  crust: 'thin',
+  size: 'small',
+  toppings: 3
+};
+
+
+/*
+In JavaScript, almost "everything" is an object.
+
+    Booleans can be objects (if defined with the new keyword)
+    Numbers can be objects (if defined with the new keyword)
+    Strings can be objects (if defined with the new keyword)
+    Dates are always objects
+    Maths are always objects
+    Regular expressions are always objects
+    Arrays are always objects
+    Functions are always objects
+    Objects are always objects
+
+All JavaScript values, except primitives, are objects.
+
+JavaScript Primitives
+A primitive value is a value that has no properties or methods.
+A primitive data type is data that has a primitive value.
+JavaScript defines 5 types of primitive data types:
+
+    string
+    number
+    boolean
+    null
+    undefined
+
+Primitive values are immutable (they are hardcoded and therefore cannot be changed).
+*/
+
+
